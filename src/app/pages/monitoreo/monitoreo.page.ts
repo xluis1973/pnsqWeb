2b134884391a0ff08694dbf528b997034d46261d
xluis1973 <50953075+xluis1973@users.noreply.github.com>
2021-11-07 02:31:59 -0300

    Mapa PNSQL Completo
    usando archivo kml

--- src/app/pages/monitoreo/monitoreo.page.ts
+++ src/app/pages/monitoreo/monitoreo.page.ts
@@ -1,6 +1,8 @@
 
 import {  Component, OnInit } from '@angular/core';
 
+import { Marker } from '../../interfaces/interfaces';
+
 
 const url ="https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml";
 
@@ -15,6 +17,41 @@ declare var google;
 
 export class MonitoreoPage implements OnInit {
 map=null;
+markers: Marker[]=[
+
+  {
+    position: {
+      lat: -33.1742696,
+      lng: -66.2954034
+    },
+    title: 'visitante 1'
+  },
+
+  {
+    position: {
+      lat: -33.1740215,
+      lng: -66.2975196
+    },
+    title: 'visitante 2'
+  },
+  {
+    position: {
+      lat: -33.1742500,
+      lng: -66.2954034
+    },
+    title: 'visitante 3'
+  },
+  {
+    position: {
+      lat: -33.1742400,
+      lng: -66.2954034
+    },
+    title: 'visitante 4'
+  }
+
+];
+
+
 public btSelectM:String="outline";
 public btSelectS:String="outline";
 public btSelectH:String="outline";
@@ -42,21 +79,47 @@ public btSelectH:String="outline";
 
   loadMap(){
     const mapEle:HTMLElement = document.getElementById('map');
-    const myLatLng={lat: -33.1807797, lng: -66.3179661};
+    const myLatLng={lat: -32.473504, lng: -66.9727219};
 
     this.map=new google.maps.Map(mapEle, {
       center: myLatLng,
       zoom: 12
     });
 
+
+  var ctaLayer = new google.maps.KmlLayer({
+      url: 'https://drive.google.com/uc?id=1aUAxnV5IBJoZnCLK3-qJFfUoZpsqSlpA',
+     suppressInfoWindows: true,  
+        map:this.map,
+        zindex: 0,
+          clickable : false
+    }); 
+
     google.maps.event.addListenerOnce(this.map,'idle',()=>{
       
       mapEle.classList.add('show-map');
+      this.renderMarkets();
     });
+       
+  }
 
+  renderMarkets(){
+    this.markers.forEach(marker=>{
     
+      this.addMarker(marker);
+      
+    });
+  }
+
+  addMarker(marker:Marker){
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
   }
   
+ 
   }
 
 
